d6993404d492db27a78ce2547b5823bb38a8331a
tiko-manukyan <tikomanukyan44@gmail.com>
2023-10-20 02:03:56 +0400

    created table with songs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+
+export interface PeriodicElement {
+  name: string;
+  id: number;
+  artist: string;
+  url: string
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {id: 1, name: 'Sister Golden Hair', artist: 'Some Guys', url: 'https://www.jonathancoulton.com/wp-content/uploads/encodes/Some_Guys/mp3/01_Sister_Golden_Hair_mp3_8961f17.mp3'},
+  {id: 2, name: 'Wake Up', artist: 'Solid State', url: 'https://www.jonathancoulton.com/wp-content/uploads/encodes/Solid_State/mp3/01_Wake_Up_mp3_a024ffd.mp3'},
+  {id: 3, name: 'Glasses', artist: 'Artificial Heart', url: 'https://www.jonathancoulton.com/wp-content/uploads/encodes/Artificial_Heart/mp3/07_Glasses_mp3_048e1ad.mp3'},
+  {id: 4, name: 'I\'m Having a Party', artist: 'Smoking Monkey', url: 'https://www.jonathancoulton.com/wp-content/uploads/encodes/Smoking_Monkey/mp3/01_Im_Having_a_Party_mp3_3da9071.mp3'},
+  {id: 5, name: 'Octopus', artist: 'The Aftermath', url: 'https://www.jonathancoulton.com/wp-content/uploads/encodes/The_Aftermath/mp3/01_Octopus_mp3_29210e9.mp3'},
+];
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +23,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tracksTable';
+  displayedColumns: string[] = ['id', 'artist',  'name'];
+  dataSource = ELEMENT_DATA;
+  @ViewChild('audioOption') audioOption!: ElementRef;
+  audioPath!: string
+
+  onPlaySound(audioSrc: string) {
+    this.audioPath = audioSrc
+    this.audioOption?.nativeElement.load()
+    this.audioOption?.nativeElement.play()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from "@angular/material/table";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
